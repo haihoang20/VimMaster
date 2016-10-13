9ee60b578e8ec0b62ca03e9e9540b0cee9600e7e
michaelwagler <michael.wagler@outlook.com>
2016-10-13 12:11:12 -0700

    added boilerplate natural language stuff from lecture file

---
+++ VimMaster.pl
@@ -0,0 +1,88 @@
+% noun_phrase(T0,T4,Ind,C0,C4) is true if
+%  T0 and T4 are list of words, such that
+%        T4 is an ending of T0
+%        the words in T0 before T4 (written T0-T4) form a noun phrase
+%  Ind is the individual that the noun phrase is referring to
+%  C0 and C4 are lists of relations such that
+%        C0 is an ending of C4 and
+%        the relations in C4-C0 give the constraints on Ind implied by the noun phrase
+% A noun phrase is a determiner followed by adjectives followed
+% by a noun followed by an optional modifying phrase:
+noun_phrase(T0,T4,Ind,C0,C4) :-
+    det(T0,T1,Ind,C0,C1),
+    adjectives(T1,T2,Ind,C1,C2),
+    noun(T2,T3,Ind,C2,C3),
+    mp(T3,T4,Ind,C3,C4).
+
+% Try:
+%?- noun_phrase([a,tall,student],T1,I1,[],C1).
+%?- noun_phrase([a,math,course],T2,I2,[],C2).
+%?- noun_phrase([a,tall,student,enrolled,in,a,math,course],T3,I3,[],C3).
+
+% Determiners (articles) are ignored in this oversimplified example.
+% They do not provide any extra constaints.
+det([the | T],T,_,C,C).
+det([a | T],T,_,C,C).
+det(T,T,_,C,C).
+
+% Adjectives consist of a sequence of adjectives.
+% The meaning of the arguments is the same as for noun_phrase
+adjectives(T0,T2,Ind,C0,C2) :-
+    adj(T0,T1,Ind,C0,C1),
+    adjectives(T1,T2,Ind,C1,C2).
+adjectives(T,T,_,C,C).
+
+% An optional modifying phrase / relative clause is either
+% a relation (verb or preposition) followed by a noun_phrase or
+% 'that' followed by a relation then a noun_phrase or
+% nothing 
+mp(T0,T2,I1,C0,C2) :-
+    reln(T0,T1,I1,I2,C0,C1),
+    noun_phrase(T1,T2,I2,C1,C2).
+mp([that|T0],T2,I1,C0,C2) :-
+    reln(T0,T1,I1,I2,C0,C1),
+    noun_phrase(T1,T2,I2,C1,C2).
+mp(T,T,_,C,C).
+
+% DICTIONARY
+
+% adj(T0,T1,Ind,C0,C1) is true if T0-T1 is an adjective that provides properties C1-C0 to Ind
+% example: adj([spanish, speaking | T],T,Ind,C,[spanish_speaking(Ind)|C]).
+
+% noun(T0,T1,Ind,C0,C1) is true if T0-T1 is a noun that provides properties C1-C0 to Ind
+% example: noun([country | T],T,Ind,C,[country(Ind)|C]).
+% The following are for proper nouns:
+% example: noun([Ind | T],T,Ind,C,C) :- country(Ind).
+
+% reln(T0,T1,I1,I2,R0,R1) is true if T0-T1 is a relation
+%   that provides relations R1-R0 on individuals I1 and I2
+% example: reln([borders | T],T,I1,I2,C,[borders(I1,I2)|C]).
+
+question([is | T0],T2,Ind,C0,C2) :-
+    noun_phrase(T0,T1,Ind,C0,C1),
+    mp(T1,T2,Ind,C1,C2).
+question([what,is | T0],T1,Ind,C0,C1) :-
+    mp(T0,T1,Ind,C0,C1).
+question([what,is | T0],T1,Ind,C0,C1) :-
+    noun_phrase(T0,T1,Ind,C0,C1).
+question([what,is | T0],T1,Ind,C0,C1) :-
+    adjectives(T0,T1,Ind,C0,C1).
+question([what | T0],T2,Ind,C0,C2) :-      % allows for a "what ... is ..."
+    noun_phrase(T0,[is|T1],Ind,C0,C1),
+    mp(T1,T2,Ind,C1,C2).
+question([what | T0],T2,Ind,C0,C2) :-
+    noun_phrase(T0,T1,Ind,C0,C1),
+    mp(T1,T2,Ind,C1,C2).
+
+% ask(Q,A) gives answer A to question Q
+ask(Q,A) :-
+    question(Q,[],A,[],C),
+    prove_all(C).
+
+% prove_all(L) proves all elements of L against the database
+prove_all([]).
+prove_all([H|T]) :-
+     H,
+    prove_all(T).
+
+
