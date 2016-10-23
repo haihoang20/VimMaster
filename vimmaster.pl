69c9583ec484bcfc125cc659a148a686468d5bcb
michaelwagler <michael.wagler@outlook.com>
2016-10-22 19:04:11 -0700

    added help message if you do vim_help(), and made it so ask just accepts one param, the question, so no need to do ask(Q, A), but just ask(Q)

--- vimmaster.pl
+++ vimmaster.pl
@@ -1,7 +1,8 @@
+% http://wiki.ubc.ca/Course:CPSC312-2016-Project1-VimMaster
 main :- 
     current_prolog_flag(argv, Argv),
     atomic_list_concat(Argv,' ',Q),
-    ask(Q, A),
+    ask_helper(Q, A),
     print(A),
     halt(0).
 
@@ -39,6 +40,7 @@ connector(T,T,_,C,C).
 
 % Attributes
 % for any given question topic, associate it with the appropriate attributes
+% eg: attribute(Topic) means that Topic is associated with attribute, and attribute should therefore be a keyword that points to Topic.
 copy(howtocopyandpasteselected).
 copy(howtocopyandpasteline).
 paste(howtocopyandpasteselected).
@@ -50,6 +52,8 @@ delete(howtodeleteline).
 move(howtomove).
 
 file(howtojumpfile).
+
+help_attribute(generalhelp).
 
 jump(howtojumpline).
 jump(howtojumpfile).
@@ -114,8 +118,8 @@ screen(howtosplitwindow).
 vertically(howtosplitwindowvertically).
 horizontally(howtosplitwindowhorizontally).
 
-
 % solution(Topic,A) gives you the solution string A associated with a Topic
+solution(generalhelp, "Welcome to Vimmaster! ask your vim questions here with the 'ask' predicate, like ask('how do you move?'), or ask('can you jump to the end of the file in vim?'). Or, if you want to learn about automatically generating a vimrc file, try vimrc_help().").
 solution(howtomove, "Move left: 'h', move right: 'l', move up: 'k', move down: 'j'. You can also move using the arrow keys.").
 solution(howtocopyandpasteselected, "To copy selected text, enter visual mode, select the text by moving the cursor until you cover it, and then type 'y'. Exit visual mode. Paste it by typing 'p'.").
 solution(howtocopyandpasteline, "To copy the current line, make sure you are in normal mode, and then type 'yy'. You can optionally type a number first, like '3 yy', and this will copy that number of consecutive lines (in this case, 3). To paste, type 'p'.").
@@ -128,8 +132,8 @@ solution(howtojumpparagraph, "To jump to the beginning of the paragraph, type '{
 solution(howtojumpword, "To jump to the beginning of the word, type 'w'. To jump to the end of the word, type 'b'.").
 
 solution(howtoinsert, "To insert before the cursor, type 'i'. To insert after the cursor, type 'a'.").
-solution(howtoinserttostartofline, "To insert to beginning of line, type 'I'.").
-solution(howtoinserttoendofline, "To insert to end of line, type 'A'.").
+solution(howtoinserttostartofline, "To insert at the beginning of the line, type 'I'.").
+solution(howtoinserttoendofline, "To insert to the end of the line, type 'A'.").
 solution(howtoinserttomultiplelines, "To insert to multiple lines, press Ctrl-v to enter visual block mode, press 'j' or 'k' to select lines to insert, type 'I', write your text, then press escape.").
 solution(howtosearchandreplace, "To search and replace, type ':s/<word-to-search>/<word-to-replace>'. You can also add a global and confirmation prompt by adding 'g' and 'c' flags. For instance, ':/s/foo/bar/gc' would replace every instance of foo with bar in the current file with a prompt specifying whether or not you want the replacement to occur.").
 solution(howtosearch, "To search, type '/<word-to-search>'. For instance '/hello', searches for the word hello in the current document.").
@@ -150,11 +154,12 @@ keyword([up | T],T,Ind,C,[up(Ind)|C]).
 keyword([copy | T],T,Ind,C,[copy(Ind)|C]).
 keyword([paste | T],T,Ind,C,[paste(Ind)|C]).
 keyword([delete | T],T,Ind,C,[delete(Ind)|C]).
+keyword([help | T],T,Ind,C,[help_attribute(Ind)|C]).
 
 keyword([selected | T],T,Ind,C,[selected(Ind)|C]).
 keyword([jump | T],T,Ind,C,[jump(Ind)|C]).
 keyword([go | T],T,Ind,C,[jump(Ind)|C]).
-keyword([bottom | T],T,Ind,C,[bottom(Ind)|C]).
+keyword([bottom | T],T,Ind,C,[end(Ind)|C]).
 
 keyword([insert | T],T,Ind,C,[insert(Ind)|C]).
 keyword([end | T],T,Ind,C,[end(Ind)|C]).
@@ -193,7 +198,7 @@ keyword([horizontally | T],T,Ind,C,[horizontally(Ind)|C]).
 keyword([vertically | T],T,Ind,C,[vertically(Ind)|C]).
 
 
-% if these words appear, they won't affect anything:
+% if these words appear, they will be ignored.
 keyword([number | T],T,_,C,C).
 keyword([beginning | T],T,_,C,C).
 keyword([start | T],T,_,C,C).
@@ -215,15 +220,15 @@ question([how, to | T0],T1,Ind,C0,C1) :-
 question(T0,T1,Ind,C0,C1) :-
     keyword_phrase(T0,T1,Ind,C0,C1).
 
-% ask(Q,A) gives answer A to question Q
-%
-% NOTE: S is a sentence, Q is that sentence as a list
-%
 % taken from http://stackoverflow.com/questions/19736439/delete-character-from-string-in-prolog
+% remove_char(S, C, X): removes character C from string S, and puts the result in X.
 remove_char(S,C,X) :- atom_concat(L,R,S), atom_concat(C,W,R), atom_concat(L,W,X).
 remove_char(S,_,S).
 
-ask(Q,T) :-
+% ask_helper(Q, T): T is the answer to the question given by string Q.
+% example queries:
+% ask_helper("how do you move",X).
+ask_helper(Q,T) :-
     remove_char(Q, '?', Q2),
     string_lower(Q2, Q3),
     atomic_list_concat(L,' ', Q3),
@@ -231,15 +236,22 @@ ask(Q,T) :-
     prove_all(C),
     solution(A, T).
 
+% ask(Q): prints the answer to question Q
+% example query:
+% ask("how do you quit?").
+ask(Q):-
+  ask_helper(Q, T),
+  print(T).
+
 % prove_all(L) proves all elements of L against the database
 prove_all([]).
 prove_all([H|T]) :-
      H,
     prove_all(T).
 
-% example queries:
-% ask("how do you move",X).
-
+% vim_help() prints out a message that gives general help advice about how to use vimmaster.
+vim_help() :-
+    ask('help').
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % vimrc_pair(X,Y) writes property Y into .vimrc file specified by X
@@ -248,8 +260,6 @@ vimrc_pair('syntax highlighting', 'syntax on').
 vimrc_pair('highlight search results', 'set hlsearch').
 vimrc_pair('auto indent', 'set autoindent').
 vimrc_pair('enable mouse', 'set mouse=a').
-vimrc_pair('search and replace word under cursor', ':nnoremap <Leader>s :%s/\<<C-r><C-w>\>//gc<Left><Left><Left>
-').
 vimrc_pair('vim colours', 'set t_Co=256').
 vimrc_pair('80 char indicator', 'set colorcolumn=80').
 vimrc_pair('highlight current line', 'set cursorline').
@@ -302,10 +312,12 @@ writeFeature([H|T], Stream) :-
 
 % prints out what you can query to make a vimrc file
 vimrc_help() :-
-    print('To generate a vimrc file, call the predicate create_vimrc, passing in a list of the features you want to include. Here is a list of all the features that you can add:'),
+    print('To generate a vimrc file, call the predicate create_vimrc, passing in a list of the features you want to include. Here is a list of all the features that you can choose from:'),
+    nl(),
     nl(),
     getAllFeatures(L),
     print(L),
+    nl(),
     nl(),
     print('So for example, try calling create_vimrc("line numbers, highlight search results") to generate a vimrc which has the rule set for causing line numbers to appear on the left margin, and also the rule that will highlight any string that is matched in a search.').
     
